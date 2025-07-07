5e75dc03ae3adc1b5b76b474058502b6e47a0f8a
James Carter <jamesascarter@gmail.com>
2025-07-07 17:01:17 +0100

    model rearrange

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class AudioCNN(nn.Module):
         super(AudioCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 16 * 16, 128)
+        self.fc1 = nn.Linear(64 * 16 * 8, 128)
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
@@ -15,7 +15,7 @@ class AudioCNN(nn.Module):
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-        x = x.view(-1, 64 * 16 * 16)
+        x = x.view(-1, 64 * 16 * 8)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
